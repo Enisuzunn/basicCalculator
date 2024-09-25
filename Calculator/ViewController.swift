89470e224603463c1eb63e3c22b85134af47bac5
Enisuzunn <enisbesiktasli@gmail.com>
2024-09-25 20:15:16 +0300

    First Commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ilkText: UITextField!
+    @IBOutlet weak var ikinciText: UITextField!
+    @IBOutlet weak var sonucLabel: UILabel!
+    var sonuc = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toplama_tiklandi(_ sender: Any) {
+        if let ilksayi = Int(ilkText.text!){
+            if let ikincisayi = Int(ikinciText.text!){
+                sonuc = ilksayi + ikincisayi
+                sonucLabel.text = String("Sonuç: \(sonuc)")
+            }
+        }
+        
+    }
+    
+    @IBAction func cikarma_tiklandi(_ sender: Any) {
+        if let ilksayi = Int(ilkText.text!){
+            if let ikincisayi = Int(ikinciText.text!){
+                sonuc = ilksayi - ikincisayi
+                sonucLabel.text = String("Sonuç: \(sonuc)")
+            }
+        }
+    }
+    @IBAction func carpma_tiklandi(_ sender: Any) {
+        if let ilksayi = Int(ilkText.text!){
+            if let ikincisayi = Int(ikinciText.text!){
+                sonuc = ilksayi * ikincisayi
+                sonucLabel.text = String("Sonuç: \(sonuc)")
+            }
+        }
+    }
+    @IBAction func bölme_tiklandi(_ sender: Any) {
+        if let ilksayi = Int(ilkText.text!){
+            if let ikincisayi = Int(ikinciText.text!){
+                sonuc = Int(ilksayi / ikincisayi)
+                sonucLabel.text = String("Sonuç: \(sonuc)")
+            }
+        }
+    }
 }
+
 
